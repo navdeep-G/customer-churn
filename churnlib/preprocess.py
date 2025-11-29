70f39fd9d55f3c2d39afa689504facfafb9d981c
navdeep-G <mr.navdeepgill@gmail.com>
2025-11-29 09:51:38 -0800

    Attempt 3

--- churnlib/preprocess.py
+++ churnlib/preprocess.py
@@ -2,7 +2,7 @@ from typing import List
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 import inspect
 
 
